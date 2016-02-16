f7eab2a4ca5923cb36e13b88e21eac0bd68044cf
johno4321 <josullivan@gmail.com>
2016-02-16 17:26:46 +0000

    checking in code

---
+++ CopyDirectoryStructure.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+
+namespace CopyDirectoryStructure
+{
+    public class CopyDirectoryStructure
+    {
+        private readonly string _sourceRootDirectory;
+        private readonly string _targetRootDirectory;
+
+        public static void Main(string[] args)
+        {
+            if(args.Length != 2)
+            {
+                Console.WriteLine("Usage: CopyDirectoryStructure <source root directory path> <target root directory path>");
+                return;
+            }
+
+            if(!Directory.Exists(args[0]))
+            {
+                Console.WriteLine("The source directory {0} does not exist", args[0]);
+                return;
+            }
+
+            if (Directory.Exists(args[1]))
+            {
+                Console.WriteLine("The target directory {0} exists and I don't overwrite", args[1]);
+                return;
+            }
+
+
+            var program = new CopyDirectoryStructure(args[0], args[1]);
+
+            program.Go();
+        }
+
+        public CopyDirectoryStructure(string sourceRootDirectory, string targetRootDirectory)
+        {
+            _sourceRootDirectory = sourceRootDirectory;
+            _targetRootDirectory = targetRootDirectory;
+        }
+
+        public void Go()
+        {
+            Console.WriteLine("About to create directory {0}", _targetRootDirectory);
+            Directory.CreateDirectory(_targetRootDirectory);
+            BuildChildDirectories(_sourceRootDirectory, _targetRootDirectory);
+        }
+
+        public void BuildChildDirectories(string sourceParentDirectory, string targetParentDirectory)
+        {
+            var sourceRootDirectoryInfo = Directory.GetDirectories(sourceParentDirectory);
+            
+            foreach(var childDirectory in sourceRootDirectoryInfo)
+            {
+                var newChildDirectoryName = Path.GetFileName(childDirectory);
+                var newChildDirectoryPath = Path.Combine(targetParentDirectory, newChildDirectoryName);
+
+                Console.WriteLine("About to create directory {0}", newChildDirectoryPath);
+
+                Directory.CreateDirectory(newChildDirectoryPath);
+
+                BuildChildDirectories(childDirectory, newChildDirectoryPath);
+            }
+        }
+    }
+}
